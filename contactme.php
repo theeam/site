e8b5b9a41333a88f502b16a2a1a62b0367ec44da
theeam <eduuardomuller@gmail.com>
2016-07-25 21:38:03 -0300

    Add files via upload

---
+++ contactme.php
@@ -0,0 +1,7 @@
+<h2>Do not hesitate to contact me</h2>
+<a href="http://www.linkedin.com/in/eduardoamuller"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
+<a href="https://github.com/theeam"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
+<a href="https://gitlab.com/u/theeam"><i class="fa fa-gitlab fa-3x" aria-hidden="true"></i></a>
+<a href="mailto:eduuardomuller@gmail.com?subject=Contact from WebGIT" "email me"><i class="fa fa-share fa-3x" aria-hidden="true"></i></a>
+<a href="http://www.facebook.com/eduardoamuller"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
+<a href="http://www.instagram.com/eduardoamuller"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
---
+++ home.php
@@ -0,0 +1,2 @@
+<h2>Welcome to my personal website!</h2>
+<p id="pe">Enjoy it</p>

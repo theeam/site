8a9e86f8708be5133045dd147f8b1348a61fcd20
theeam <eduuardomuller@gmail.com>
2016-07-26 21:53:11 -0300

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -1,7 +1,40 @@
-<h2>Do not hesitate to contact me</h2>
-<a href="http://www.linkedin.com/in/eduardoamuller"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
-<a href="https://github.com/theeam"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
-<a href="https://gitlab.com/u/theeam"><i class="fa fa-gitlab fa-3x" aria-hidden="true"></i></a>
-<a href="mailto:eduuardomuller@gmail.com?subject=Contact from WebGIT" "email me"><i class="fa fa-share fa-3x" aria-hidden="true"></i></a>
-<a href="http://www.facebook.com/eduardoamuller"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
-<a href="http://www.instagram.com/eduardoamuller"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Eduardo Albuquerque Müller</title>
+	<link rel="stylesheet" href="stylesheet.css" type="text/css">
+	<link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+</head>
+<body>
+	<header>
+		<h1 id="th1">Eduardo Albuquerque Müller</h1>
+		<nav id="navigation">
+			<a href="?pagina=home">Home</a>
+			<a href="?pagina=aboutme">About me</a>
+			<a href="?pagina=work">Work</a>
+			<a href="?pagina=contactme">Contact me</a>
+		</nav>
+	</header>
+	<div>
+			<h2>Do not hesitate to contact me</h2>
+			<a href="http://www.linkedin.com/in/eduardoamuller"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
+			<a href="https://github.com/theeam"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
+			<a href="https://gitlab.com/u/theeam"><i class="fa fa-gitlab fa-3x" aria-hidden="true"></i></a>
+			<a href="mailto:eduuardomuller@gmail.com?subject=Contact from WebGIT" "email me"><i class="fa fa-share fa-3x" aria-hidden="true"></i></a>
+			<a href="http://www.facebook.com/eduardoamuller"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
+			<a href="http://www.instagram.com/eduardoamuller"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>	
+	</div>
+
+
+
+	<footer>
+		<p align="right">Designed by:TheEAM</p>
+	</footer>
+
+
+	
+</body>
+</html>
+
